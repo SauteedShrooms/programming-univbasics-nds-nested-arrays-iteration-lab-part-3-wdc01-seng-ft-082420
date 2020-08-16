a2f1c9a3385df815396430508a9ee97a6f7261f6
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-16 23:33:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,11 +8,9 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do
       if src[row_index][element_index].is_a? String
         string_array << src[row_index][element_index]
-        
-      string_total += src[row_index][element_index]
       element_index += 1
     end
     row_index += 1
   end
-  string_total
+  string_array
 end
